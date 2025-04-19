fd33c96a0355c79e705967457504ce4c19594ceb
Arby S. <59036227+rb2kGit@users.noreply.github.com>
2025-04-19 00:26:47 -0400

    Added grounded detection.

--- My project/Assets/Scripts/playerController.cs
+++ My project/Assets/Scripts/playerController.cs
@@ -2,31 +2,58 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    //Character state variables.
-    private bool isGrounded;
-    public Vector2 playerPosition;
+    //Level Variables
+    public LayerMask groundLayer;
+
+    //Raycasting variables.
+    public Vector2 boxCastSize;
+    public float boxCastDistance;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        
+    }
+    
     void Start()
     {
-        playerPosition = new Vector2(transform.position.x, transform.position.y);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        groundCheck(playerPosition, isGrounded);
+        
     }
 
-    public void groundCheck(Vector2 playerPosition, bool isGrounded)
+    void FixedUpdate()
     {
-        //Initialize method variables.
-        Vector2 position = playerPosition + new Vector2(0, -0.01f); //Added offset of -0.01 to Y axis. Need this to use overlap detection method.
         
-        //Bookmark--> isGrounded = Physics2D.OverlapBox();
+    }
 
-        
+
+    public bool groundedCheck()
+    {
+        //Raycast a box to detect a collision with the ground layer. 
+        if(Physics2D.BoxCast(transform.position, boxCastSize, 0, -transform.up, boxCastDistance, groundLayer, 0, 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 
     }
+
+    private void OnDrawGizmos()
+    {
+        //Cahnge the gizmo colors to red.
+        Gizmos.color = Color.red;
+        
+        //Draw a wire square that matches the location and size of the boxCast used for grounded checking.
+        Gizmos.DrawWireCube(transform.position - transform.up * boxCastDistance, boxCastSize);
+    }
+
 }
