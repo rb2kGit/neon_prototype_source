1fc5426e4e474b2ac0ae5c6b23bfd56cd9b22c77
Arby S. <59036227+rb2kGit@users.noreply.github.com>
2025-04-26 23:32:23 -0400

    Modified jump, added platform recognition to grounded check.

--- My project/Assets/Scripts/playerController.cs
+++ My project/Assets/Scripts/playerController.cs
@@ -8,6 +8,9 @@ public class playerController : MonoBehaviour
     public float jumpSpeed;
     public float accelSpeed;
     public float decelSpeed;
+
+    [SerializeField]
+    private float groundMemory;
     
     //Input Variables
     private float xInput;
@@ -17,6 +20,7 @@ public class playerController : MonoBehaviour
 
     //Level Variables
     public LayerMask groundLayer;
+    public LayerMask platformLayer;
 
     //Raycasting variables.
     public Vector2 boxCastSize;
@@ -59,18 +63,19 @@ public class playerController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             jumpInput = true; //Record jump as true to be used in jump execution method.
-            jumpMemory = 0.2f; //Set the jump memeory timer.
+            jumpMemory = 0.15f; //Set the jump memory timer.
         }
         
     }
 
-    private void moveHandler() //<---Left off. Figuring out how to dampen movement when jumping straight up.
+    private void moveHandler()
     {
         //Initialize local variables.
         float currentVelocity = rig.linearVelocity.x; //Create a reference variable for the current velocity.
         float accelerationCap = accelSpeed * Time.fixedDeltaTime; //This varable will use the accelaration speed to create an accelartion cap in Mathf.MoveTowards, when combined with time.delta time.
         float deccelerationCap = decelSpeed * Time.fixedDeltaTime; //This varable will use the decceleration speed to create an decceleration cap in Mathf.MoveTowards, when combined with time.delta time.
-        float airDampening = (accelSpeed * 0.6f) * Time.fixedDeltaTime; //This varable will use the airDampening speed to create an airDempening cap in Mathf.MoveTowards, when combined with time.delta time.
+        float airDampening = (accelSpeed * 0.5f) * Time.fixedDeltaTime; //This varable will use the airDampening speed to create an airDempening cap in Mathf.MoveTowards, when combined with time.delta time.
+        float stillAirDampening = (accelSpeed * 0.1f) * Time.fixedDeltaTime; //This varable will use the airDampening speed to create an airDempening cap in Mathf.MoveTowards, when combined with time.delta time.
 
         if(groundedCheck() && xInput != 0) //When the player is not pressing left or right on the ground.
         {
@@ -90,33 +95,29 @@ public class playerController : MonoBehaviour
             rig.linearVelocity = new Vector2(Mathf.MoveTowards(currentVelocity, xInputMemory * moveSpeed, airDampening ), rig.linearVelocity.y);
             
         }
+        else if(rig.linearVelocity.x < 0.5 * moveSpeed && rig.linearVelocity.y > 0) //When the player is rising with a slow speed, heavily dampen x velocity. Will make standstill jumping feel realistic.
+        {
+            rig.linearVelocity = new Vector2(Mathf.MoveTowards(currentVelocity, xInputMemory * moveSpeed, stillAirDampening ), rig.linearVelocity.y);
+            xInputMemory = xInput;
+
+        }
         else //When the player is in the air.
         {
             rig.linearVelocity = new Vector2(Mathf.MoveTowards(currentVelocity, xInput * moveSpeed, airDampening ), rig.linearVelocity.y);
             xInputMemory = xInput;
         }
-        Debug.Log(xInputMemory);
-        /*else if(Mathf.Sign(rig.linearVelocity.x) != xInput)
-        {
-            rig.linearVelocity = new Vector2(xInput * airControlSpeed, rig.linearVelocity.y);
-        }
-        else if(rig.linearVelocity.x == 0)
-        {
-            rig.linearVelocity = new Vector2(xInput * airControlSpeed, rig.linearVelocity.y); //<------------------------ Pickup here. Figure out how player can change direction in mid air.
-        }*/
-
-        //horizontalVelocity  += xInput
-        //horizontalVelocity *= Mathf.Pow(1f - horizontalDampTurning, moveSpeed * 10f);
         
     }
 
     private void jumpHandler()
     {
-        //The jump will only be execute when the user is grounded, if the jump input has been pressed, within the remaining amount of jump memory time.
-       if(groundedCheck() && jumpInput && jumpMemory > 0)
+        //The jump will only be executed when the user is grounded, if the jump input has been pressed, within the remaining amount of jump memory time.
+       if(jumpInput && jumpMemory > 0 && groundMemory > 0)
        {
             rig.linearVelocity = new Vector2(rig.linearVelocity.x, jumpSpeed);
             jumpInput = false;
+            jumpMemory = 0;
+            groundMemory = 0;
        }
 
     }
@@ -124,8 +125,9 @@ public class playerController : MonoBehaviour
     public bool groundedCheck()
     {
         //Raycast a box to detect a collision with the ground layer. 
-        if(Physics2D.BoxCast(transform.position, boxCastSize, 0, -transform.up, boxCastDistance, groundLayer, 0, 0))
+        if(Physics2D.BoxCast(transform.position, boxCastSize, 0, -transform.up, boxCastDistance, groundLayer, 0, 0 ) || Physics2D.BoxCast(transform.position, boxCastSize, 0, -transform.up, boxCastDistance, platformLayer, 0, 0 ))
         {
+            Debug.Log("Grounded");
             return true;
         }
         else
@@ -146,6 +148,17 @@ public class playerController : MonoBehaviour
         {
             jumpInput = false; //When the jump memory timer runs out. Set jump input to false.
         }
+
+        //Decrement the grounded memory timer by time.delta time.
+        if(!groundedCheck() && groundMemory > 0)
+        {
+            groundMemory = Mathf.Clamp(groundMemory, 0f, 0.15f) - Time.deltaTime;
+        }
+        else if(groundedCheck() && groundMemory <= 0)
+        { 
+            groundMemory = 0.15f;
+        }
+        Debug.Log(groundMemory);
     }
 
     private void OnDrawGizmos()
